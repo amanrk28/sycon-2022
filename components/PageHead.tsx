2154972e091d249a9c3b9ffd423c95298220a409
AmanRK <amankhemka.ak28@gmail.com>
2022-01-19 12:55:25 +0530

    initial commit

---
+++ components/PageHead.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+interface PageHeadProps {
+  title: string;
+  description: string;
+}
+
+export default function PageHead({ title, description }: PageHeadProps) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { AuthProvider } from '../lib/AuthProvider';
+import { Toaster } from 'react-hot-toast';
+import '../styles/globals.css';
+
+const toastOptions = {
+  duration: 4000,
+  style: {
+    background: '#333333',
+    color: '#FFFFFF',
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+      <Toaster position="top-center" toastOptions={toastOptions} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
